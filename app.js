62278e2b9c124b062faec9d216157a20d4e66443
Class Now <nazmuledu0@gmail.com>
2026-05-23 20:15:21 +0600

    Update app.js

--- app.js
+++ app.js
@@ -520,3 +520,15 @@ function signup() {
       alert(error.message);
     });
 }
+function login() {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(function() {
+      window.location.href = "classes.html";
+    })
+    .catch(function(error) {
+      alert(error.message);
+    });
+}
